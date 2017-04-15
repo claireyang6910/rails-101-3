ff8bb2ab0d19e05592fe2d94c8d468c7f53dfdfa
yangyihan <claireyang69@163.com>
2017-04-15 15:17:03 +0800

    add post button

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -40,7 +40,7 @@ class GroupsController < ApplicationController
     @group.destroy
     redirect_to groups_path, alert: "Group deleted"
   end
-  
+
   private
 
   def find_group_and_check_permission
@@ -48,10 +48,8 @@ class GroupsController < ApplicationController
 
     if current_user != @group.user
       redirect_to root_path, alert: "You have no permission."
-    end
+  end
 
   def group_params
     params.require(:group).permit(:title, :description)
   end
-
-end
